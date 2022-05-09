8b1ce7b485930da2503699c027e2caa92290ea8b
fiamardar <stefania.mardar@gmail.com>
2022-05-09 20:57:03 +0300

    Added Add Test Building

--- src/test/java/com/example/TDTP/AddNewBuildingTest.java
+++ src/test/java/com/example/TDTP/AddNewBuildingTest.java
@@ -3,12 +3,14 @@ package com.example.TDTP;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.util.List;
 
 public class AddNewBuildingTest {
